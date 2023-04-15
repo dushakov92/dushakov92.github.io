96556240ee500e49b6cf9b0bf6c47c46f0e93a9c
denis ushakov <denis_ushakov@hotmail.com>
2023-04-15 15:29:11 -0500

    deploy next js to github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  assetPrefix: isProd ? '/your-github-repo-name/' : '',
+  images: {
+    unoptimized: true,
+  },
+  nextConfig
+}
