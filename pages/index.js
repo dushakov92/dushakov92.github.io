55fcf3660d206fc603054e671fc9b82ce1ae4cca
denis ushakov <denis_ushakov@hotmail.com>
2023-04-15 15:06:31 -0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <Image
+          src="/dallas.png"
+          alt="Dallas"
+          className="rounded-lg"
+          width={1280}
+          height={720}
+          priority
+        />
+      </div>
+    </main>
+  );
+}
