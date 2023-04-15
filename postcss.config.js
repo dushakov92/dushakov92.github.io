206193b009218090cb986636dff1445dabe8ed57
denis ushakov <denis_ushakov@hotmail.com>
2023-04-15 18:15:25 -0500

    updating dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   plugins: {
+    'postcss-100vh-fix': {},
+    'tailwindcss/nesting': {},
     tailwindcss: {},
-    autoprefixer: {},
-  },
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 3,
+      features: {
+        'custom-properties': false,
+        'nesting-rules': true
+      }
+    }
+  }
 }
