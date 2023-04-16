463900c83a57c589386229e78349f1524e343c53
denis ushakov <denis_ushakov@hotmail.com>
2023-04-15 19:39:24 -0500

    meta

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,27 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <Image
-          src="/dallas.png"
-          alt="Dallas"
-          className="rounded-lg"
-          width={1280}
-          height={720}
-          priority
-        />
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>DxU</title>
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+          <Image
+            src="/dallas.png"
+            alt="Dallas"
+            className="rounded-lg"
+            width={1280}
+            height={720}
+            priority
+          />
+        </div>
+      </main>
+    </>
   );
 }
