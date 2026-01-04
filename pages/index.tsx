5bea027a146374a59eeb2d9ead8abc92a2b822a8
denis ushakov <denis_ushakov@hotmail.com>
2026-01-04 16:50:24 -0600

    feat: post ai

---
+++ pages/index.tsx
@@ -0,0 +1,140 @@
+import Head from "next/head";
+import { Table } from "@radix-ui/themes";
+import Link from "next/link";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck, faBan } from '@fortawesome/free-solid-svg-icons'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home - PostAI by dxu</title>
+      </Head>
+      <main className="flex flex-col items-center justify-between px-6 py-6 md:px-12 xl:px-24">
+        <div className="w-full xl:w-2/3 flex flex-col items-center mb-4">
+          <h1 className="text-3xl">PostAI</h1>
+          <p className="text-lg">Integrate AI models to generate AI-powered content like excerpts, images, and proofreading assistance directly from the post editor.</p>
+        </div>
+          <Table.Root variant="surface" className="w-full xl:w-2/3">
+              <Table.Header>
+                <Table.Row>
+                  <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell>Free</Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell>Pro</Table.ColumnHeaderCell>
+                </Table.Row>
+              </Table.Header>
+
+              <Table.Body>
+                <Table.Row>
+                  <Table.RowHeaderCell>AI Excerpt Generation</Table.RowHeaderCell>
+                  <Table.Cell><FontAwesomeIcon className="text-green-700" icon={faCheck} size="lg" /></Table.Cell>
+                  <Table.Cell><FontAwesomeIcon className="text-green-700" icon={faCheck} size="lg" /></Table.Cell>
+                </Table.Row>
+
+                <Table.Row>
+                  <Table.RowHeaderCell>AI Image Generation</Table.RowHeaderCell>
+                  <Table.Cell><FontAwesomeIcon className="text-green-700" icon={faCheck} size="lg" /></Table.Cell>
+                  <Table.Cell><FontAwesomeIcon className="text-green-700" icon={faCheck} size="lg" /></Table.Cell>
+                </Table.Row>
+
+                <Table.Row>
+                  <Table.RowHeaderCell>OpenAI Model</Table.RowHeaderCell>
+                  <Table.Cell><FontAwesomeIcon className="text-green-700" icon={faCheck} size="lg" /></Table.Cell>
+                  <Table.Cell><FontAwesomeIcon className="text-green-700" icon={faCheck} size="lg" /></Table.Cell>
+                </Table.Row>
+
+                <Table.Row>
+                  <Table.RowHeaderCell>Gemini Model</Table.RowHeaderCell>
+                  <Table.Cell><FontAwesomeIcon className="text-green-700" icon={faCheck} size="lg" /></Table.Cell>
+                  <Table.Cell><FontAwesomeIcon className="text-green-700" icon={faCheck} size="lg" /></Table.Cell>
+                </Table.Row>
+
+                <Table.Row>
+                  <Table.RowHeaderCell>AI Content Proof Reading</Table.RowHeaderCell>
+                  <Table.Cell><FontAwesomeIcon className="text-red-700" icon={faBan} size="lg" /></Table.Cell>
+                  <Table.Cell><FontAwesomeIcon className="text-green-700" icon={faCheck} size="lg" /></Table.Cell>
+                </Table.Row>
+                
+                <Table.Row>
+                  <Table.RowHeaderCell>AI Writing Assistance</Table.RowHeaderCell>
+                  <Table.Cell><FontAwesomeIcon className="text-red-700" icon={faBan} size="lg" /></Table.Cell>
+                  <Table.Cell><FontAwesomeIcon className="text-green-700" icon={faCheck} size="lg" /></Table.Cell>
+                </Table.Row>
+
+                <Table.Row>
+                  <Table.RowHeaderCell>Front-end Widget</Table.RowHeaderCell>
+                  <Table.Cell><FontAwesomeIcon className="text-red-700" icon={faBan} size="lg" /></Table.Cell>
+                  <Table.Cell><FontAwesomeIcon className="text-green-700" icon={faCheck} size="lg" /></Table.Cell>
+                </Table.Row>
+
+                <Table.Row>
+                  <Table.RowHeaderCell>Enable for all post types or selected ones</Table.RowHeaderCell>
+                  <Table.Cell><FontAwesomeIcon className="text-red-700" icon={faBan} size="lg" /></Table.Cell>
+                  <Table.Cell><FontAwesomeIcon className="text-green-700" icon={faCheck} size="lg" /></Table.Cell>
+                </Table.Row>
+
+                <Table.Row>
+                  <Table.RowHeaderCell>Update excerpt length</Table.RowHeaderCell>
+                  <Table.Cell><FontAwesomeIcon className="text-red-700" icon={faBan} size="lg" /></Table.Cell>
+                  <Table.Cell><FontAwesomeIcon className="text-green-700" icon={faCheck} size="lg" /></Table.Cell>
+                </Table.Row>
+
+                <Table.Row>
+                  <Table.RowHeaderCell>Edit excerpt generation prompt</Table.RowHeaderCell>
+                  <Table.Cell><FontAwesomeIcon className="text-red-700" icon={faBan} size="lg" /></Table.Cell>
+                  <Table.Cell><FontAwesomeIcon className="text-green-700" icon={faCheck} size="lg" /></Table.Cell>
+                </Table.Row>
+
+                <Table.Row>
+                  <Table.RowHeaderCell>Use custom fields for enhanced AI results</Table.RowHeaderCell>
+                  <Table.Cell><FontAwesomeIcon className="text-red-700" icon={faBan} size="lg" /></Table.Cell>
+                  <Table.Cell><FontAwesomeIcon className="text-green-700" icon={faCheck} size="lg" /></Table.Cell>
+                </Table.Row>
+
+                <Table.Row>
+                  <Table.RowHeaderCell>Change image size</Table.RowHeaderCell>
+                  <Table.Cell><FontAwesomeIcon className="text-red-700" icon={faBan} size="lg" /></Table.Cell>
+                  <Table.Cell><FontAwesomeIcon className="text-green-700" icon={faCheck} size="lg" /></Table.Cell>
+                </Table.Row>
+
+                <Table.Row>
+                  <Table.RowHeaderCell>Use post excerpt as image generation prompt for enhanced AI results</Table.RowHeaderCell>
+                  <Table.Cell><FontAwesomeIcon className="text-red-700" icon={faBan} size="lg" /></Table.Cell>
+                  <Table.Cell><FontAwesomeIcon className="text-green-700" icon={faCheck} size="lg" /></Table.Cell>
+                </Table.Row>
+
+                <Table.Row>
+                  <Table.RowHeaderCell>Edit image generation prompt</Table.RowHeaderCell>
+                  <Table.Cell><FontAwesomeIcon className="text-red-700" icon={faBan} size="lg" /></Table.Cell>
+                  <Table.Cell><FontAwesomeIcon className="text-green-700" icon={faCheck} size="lg" /></Table.Cell>
+                </Table.Row>
+
+                <Table.Row>
+                  <Table.RowHeaderCell>Allow AI learn about website for enhanced AI results</Table.RowHeaderCell>
+                  <Table.Cell><FontAwesomeIcon className="text-red-700" icon={faBan} size="lg" /></Table.Cell>
+                  <Table.Cell><FontAwesomeIcon className="text-green-700" icon={faCheck} size="lg" /></Table.Cell>
+                </Table.Row>
+
+                <Table.Row>
+                  <Table.RowHeaderCell>Access additional AI models</Table.RowHeaderCell>
+                  <Table.Cell><FontAwesomeIcon className="text-red-700" icon={faBan} size="lg" /></Table.Cell>
+                  <Table.Cell><FontAwesomeIcon className="text-green-700" icon={faCheck} size="lg" /></Table.Cell>
+                </Table.Row>
+
+                <Table.Row>
+                  <Table.RowHeaderCell>Price</Table.RowHeaderCell>
+                  <Table.Cell>$0</Table.Cell>
+                  <Table.Cell>Coming Soon</Table.Cell>
+                </Table.Row>
+
+                <Table.Row>
+                  <Table.RowHeaderCell>Download</Table.RowHeaderCell>
+                  <Table.Cell><Link href="https://wordpress.org/plugins/search/post+ai/" target="_blank">Wordpress Marketplace</Link></Table.Cell>
+                  <Table.Cell>Coming Soon</Table.Cell>
+                </Table.Row>
+              </Table.Body>
+          </Table.Root>
+      </main>
+    </>
+  );
+}
