48bc78054db72182971f310e005917006dc8255f
denis ushakov <denis_ushakov@hotmail.com>
2026-01-09 12:26:09 -0600

    feat: update docs

--- components/documentation-section.tsx
+++ components/documentation-section.tsx
@@ -9,21 +9,30 @@ interface SectionContentProps {
   description?: React.ReactNode | string;
 }
 
-const SectionContent = ({ title, description }: SectionContentProps) => (
-  <div className="flex flex-col xl:flex-row xl:gap-10 border-b border-b-gray-700 pb-10">
-    {title && <h4 className="xl:w-1/4 shrink-0">{title}</h4>}
-    {description &&
-      (typeof description === "string" &&
-      /<([a-z][\s\S]*?)>/i.test(description) ? (
-        <div
-          className="flex-1"
-          dangerouslySetInnerHTML={{ __html: description }}
-        />
-      ) : (
-        <div className="flex-1">{description}</div>
-      ))}
-  </div>
-);
+const SectionContent = ({ title, description }: SectionContentProps) => {
+  if (!title && !description) return null;
+
+  const id = title?.toLowerCase().replace(/\s+/g, "-") || "";
+
+  return (
+    <div
+      id={id}
+      className="flex flex-col xl:flex-row xl:gap-10 border-b border-b-gray-700 pb-10"
+    >
+      {title && <h4 className="xl:w-1/4 shrink-0">{title}</h4>}
+      {description &&
+        (typeof description === "string" &&
+        /<([a-z][\s\S]*?)>/i.test(description) ? (
+          <div
+            className="flex-1"
+            dangerouslySetInnerHTML={{ __html: description }}
+          />
+        ) : (
+          <div className="flex-1">{description}</div>
+        ))}
+    </div>
+  );
+};
 
 export const DocumentationSection = ({
   header = "",
--- docs/setting-up-frontend-widget.tsx
+++ docs/setting-up-frontend-widget.tsx
@@ -3,7 +3,7 @@ export const settingUpFrontendWidgetDocumentation = [
     title: "Using class name",
     description:
       "<p>Add the <code>.postai-summary-trigger</code> class to an HTML element.</p>" +
-      "<p>When this element is clicked, the AI will generate a summary based on the page content and display it in a modal window, or attach it to a custom selector if specified in the settings.</p>" +
+      "<p>When this element is clicked, the AI will generate a summary based on the page content and display it in a modal window, or attach it to a <a href='#frontend-widget-selector-for-summary-content'>Frontend Widget Selector for Summary Content</a> if specified in the settings.</p>" +
       "<p>Example: <code>&lt;button class='postai-summary-trigger'&gt;Summarize Page&lt;/button&gt;</code></p>",
   },
   {
